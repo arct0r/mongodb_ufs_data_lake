af0eb89de051e8799c3305391359207a2cec4634
arct0r <109032203+arct0r@users.noreply.github.com>
2024-07-09 17:27:08 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -5,6 +5,7 @@ import pymongo
 st.set_page_config(
     page_title="Homepage",
     page_icon="👋",
+    layout='wide'
 )
 
 st.title('Quack quack motherfuckers 🐥')
@@ -20,5 +21,35 @@ st.session_state['artists'] = db['artists']
 st.session_state['events'] = db['events']
 st.session_state['locations'] = db['locations']
 st.session_state['tickets'] = db['tickets']
+
+events = db['events'].find({})
+events = [event for event in events]
+
+with st.expander('Event json'):
+    events
+
+c1, c2 = st.columns(2)
+columns = [c1,c2]
+
+def print_event(event:dict):
+            f'*Evento:* {event['event_name']}'
+            f'*Artisti:* {event['artist']}'
+            f'*Data:* {event['date']}'
+            f'*Posti disponibili:* {event['slots']}'
+            f'*Prezzo:* {event['price']} 🍌'
+            f'*Descrizione:* {event['description']}'
+            confirm_event = st.form_submit_button("Add to cart")
+
+
+for i in range(0,len(events),2):
+    with c1:
+        with st.form(f'{i}'):
+            print_event(events[i])
+    with c2:
+        try:
+            with st.form(f'{i+1}'):
+                print_event(events[i])
+        except:
+            pass
 
 
