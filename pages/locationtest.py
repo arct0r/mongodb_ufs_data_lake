d1232564f7726f79cf20a1f9f385b471936c7660
skad27 <adam.pezz@hotmail.com>
2024-07-09 17:15:17 +0200

    creazione codice converte cordinate

---
+++ pages/locationtest.py
@@ -0,0 +1,26 @@
+import stremlit as st
+from geopy.geocoders import Nominatim
+# Inizializza il geocodificatore
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+# Funzione per ottenere le coordinate geografiche
+def get_geocode(address):
+    location = geolocator.geocode(address)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        print("Impossibile ottenere le coordinate.")
+        return None, None
+# Input dall'utente
+address = input("Inserisci un indirizzo: ")
+
+# Ottieni le coordinate geografiche
+lat, lon = get_geocode(address)
+if lat and lon:
+    print(f'Le coordinate di "{address}" sono: latitudine {lat}, longitudine {lon}')
+    
+    # Converte le coordinate
+    x, y = convert_coordinates(lat, lon)
+    print(f'Le coordinate UTM sono: {x}, {y}')
+else:
+    print('Impossibile ottenere le coordinate.')
