5b5d724245e02daa115258eca933c25a2aa48d01
arct0r <109032203+arct0r@users.noreply.github.com>
2024-07-15 11:51:29 +0200

    Adding comments

--- functions.py
+++ functions.py
@@ -51,18 +51,21 @@ def filter_query(filters):
     print("Filtri in entrata:", filters)
     if 'artisti' in filters:
         query['artist'] = filters['artisti']
+        # Semplice semplice. Non serve il regex per gli artisti
     if 'nome_evento' in filters:
         query['event_name'] = {'$regex': filters['nome_evento'], '$options': 'i'}
+        # Uso il regex per il nome_evento
     if 'date' in filters:
-        start_date = datetime.datetime.combine(filters['date']['start'], datetime.time.min)
+        start_date = datetime.datetime.combine(filters['date']['start'], datetime.time.min) # Devo fare un combine per il datetime da streamlit a mongo
         end_date = datetime.datetime.combine(filters['date']['end'], datetime.time.max)
         query['date'] = {
-            '$gte': start_date,
-            '$lte': end_date
+            '$gte': start_date, # greather than starting date
+            '$lte': end_date # less than end date
         }
-    if 'luogo_filtro' in filters:
-        query['location'] = {'$regex': filters['luogo_filtro'], '$options': 'i'}
+   # if 'luogo_filtro' in filters:
+    #    query['location'] = {'$regex': filters['luogo_filtro'], '$options': 'i'}
         # $options : i era "Case insensitivity to match upper and lower cases. "
+        # Non serve piu'. Cerco direttamente le coordinate
     if 'coordinate' in filters and 'distanza' in filters:
         lon, lat = filters['coordinate']
         query['location_coordinates'] = {
@@ -95,12 +98,12 @@ def load_ticket(event):
         {
             '_id': event['_id'],
             'freeSlots': {'$gt': 0}  #$gt è 'greater than'. Si assicura che ci siano piu' di 0 tickets disponibili.
-        },
+        }, # Il primo dizionario è per "findare"
         {'$inc': {'freeSlots': -1}},
         return_document=ReturnDocument.AFTER
-    )
+    ) # Il secondo è per fargli eseguire qualcosa su ciò che ha trovato
 
-    if updated_event:
+    if updated_event: # Se ha trovato un documento e ridotto gli slots liberi...
         print(f"Sono riuscito a ridurre gli slots per {event['evento']}: {updated_event['freeSlots']} posti rimasti")
         
         # Genero un uuid per il ticket
@@ -116,10 +119,10 @@ def load_ticket(event):
             {'$push': {'tickets': new_ticket}},
             upsert=True
         ) # Questo mi 'spinge' il ticket dentro alla collections dei tickets.
-        # Se è la prima volta che viene emesso un ticket per un determinato evento allora viene creato un nuovo oggetto per quel evento dentro la collection di tickets.
+        # Se è la prima volta che viene emesso un ticket per un determinato evento allora viene creato un nuovo oggetto per quell'evento dentro la collection di tickets.
 
-        if result.modified_count > 0 or result.upserted_id:
-            print(f"Ticket {ticket_id} added to event {event['_id']}")
+        if result.modified_count > 0 or result.upserted_id: # Modified count se aggiunge, upserted_id se ha creato una nuova entry
+            print(f"Ticket {ticket_id} aggiunto all'evento {event['_id']}")
             return ticket_id
         else:
             print("Non sono riuscito ad aggiungere il ticket alla collections dei tickets")
